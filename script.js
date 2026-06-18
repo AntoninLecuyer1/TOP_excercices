24e1176df3642618d7e678705d6dc0e4382bec89
Antonin LECUYER <antonin.lecuyer.pro@gmail.com>
2026-06-18 22:58:20 +0200

    Premier commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function Book (title,author,pages,read) {
+    if (!new.target) {
+     throw Error("You must use the 'new' operator to call the constructor");
+    }
+
+    this.title=title;
+    this.author=author;
+    this.pages=pages;
+    this.read=read;
+
+    this.info = function (){
+
+        console.log(`${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? 'read' : 'not read yet'}`);
+    };
+}
+
+let theHobbit = new Book("The Hobbit" , "J.R.R Tolkien" , 295 , true);
+
+console.log(theHobbit.info());
