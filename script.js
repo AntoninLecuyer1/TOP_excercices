0b1b799c92067202faa55cc8cb451af2a353fbd5
Antonin LECUYER <antonin.lecuyer.pro@gmail.com>
2026-06-20 15:45:41 +0200

    Form creation

--- script.js
+++ script.js
@@ -1,19 +1,50 @@
-function Book (title,author,pages,read) {
-    if (!new.target) {
-     throw Error("You must use the 'new' operator to call the constructor");
-    }
+const myLibrary = [];
 
-    this.title=title;
-    this.author=author;
-    this.pages=pages;
-    this.read=read;
-
-    this.info = function (){
-
-        console.log(`${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? 'read' : 'not read yet'}`);
-    };
+function Book(id,title,author,read) {
+  this.id = id;
+  this.title = title;
+  this.author = author;
+  this.read = read;
 }
 
-let theHobbit = new Book("The Hobbit" , "J.R.R Tolkien" , 295 , true);
+function addBookToLibrary(title,author,read) {
+  // take params, create a book then store it in the array
+  const id = crypto.randomUUID();
+  const book = new Book(id,title,author,read);
+  myLibrary.push(book);
+}
 
-console.log(theHobbit.info());
+function displayBooksOfLibrary(){
+    for(let book of myLibrary){
+        console.log(`The ID : ${book.id}
+        The title : ${book.title}
+        The author : ${book.author}
+        Have you read this book : ${book.read ? "Yes I have" : "No, I haven't"}`);
+    }
+}
+
+
+
+const button = document.querySelector("#addBook");
+const formulaire = document.querySelector("#form");
+const submit = document.querySelector('#submit');
+
+button.addEventListener("click", () =>{
+    formulaire.style.display = "flex";
+    button.style.display = "none";
+});
+
+submit.addEventListener("click", () =>{
+        const titleEntered = document.querySelector("#title").value;
+        const authorEntered = document.querySelector("#author").value;
+        const readEntered = document.querySelector("#read").checked;
+
+        if(!readEntered || !authorEntered || !readEntered) {
+           console.log("Missing an entry to the form");
+            return; 
+        }
+        addBookToLibrary(titleEntered,authorEntered,readEntered.value); 
+        displayBooksOfLibrary();
+})
+
+displayBooksOfLibrary();
